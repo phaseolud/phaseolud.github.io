3ea6327431f02dec85a1ddd56779cdcd255ec3eb
Loek van Leeuwen <loekvanleeuwen@outlook.com>
2022-10-07 10:04:05 +0200

    setup for blog scp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://phasedblog.com",
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+    sitemap(),
+  ]
 });
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE: string = "Phased Blog";
+export const SITE_DESCRIPTION: string = "Welcome to my website! This is my personal blog about mathematics, nuclear fusion, programming and other science and engineering related topics.";
