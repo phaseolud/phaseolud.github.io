6cdb7517ed245f7bb40b540b3e8bdad98f571ae4
Loek van Leeuwen <loekvanleeuwen@outlook.com>
2022-10-30 10:28:58 +0100

    add correct colour map

--- src/components/blog/machine-learning-reconstruction/CameraImageCanvas.tsx
+++ src/components/blog/machine-learning-reconstruction/CameraImageCanvas.tsx
@@ -4,8 +4,9 @@ import tokamakRenderImage from "./masked_rendered_view_highres.png";
 import { colorMap } from './colorMap.js';
 
 type Props = {
-  flatCameraImage;
+  flatCameraImage: number[]
 };
+
 const scaleFactor = 0.67;
 const aspectRatio = 772 / 1032;
 const CameraImageCanvas = ({ flatCameraImage: flatCameraImage }: Props) => {
@@ -29,18 +30,21 @@ const CameraImageCanvas = ({ flatCameraImage: flatCameraImage }: Props) => {
 
   useEffect(() => {
     let colourImageData = new Uint8ClampedArray(258 * 193 * 4);
-    const maxFlatCameraImage = math.max(flatCameraImage);
-    const brightnessScaling = 380.0 / maxFlatCameraImage;
-    const normalizedColour = [1, 56 / 229, 64 / 229];
+    const maxFlatCameraImage = math.max(flatCameraImage) + 0.01; // or vMax at some point (maybe math.max * 0.8?)
+    const brightnessScaling = 255 / maxFlatCameraImage;
     for (let i = 0; i < colourImageData.length; i += 4) {
-      colourImageData[i + 0] = brightnessScaling * flatCameraImage[i / 4]; // R value
-      colourImageData[i + 1] = normalizedColour[1] * brightnessScaling * flatCameraImage[i / 4]; // G value
-      colourImageData[i + 2] = normalizedColour[2] * brightnessScaling * flatCameraImage[i / 4]; // B value
-      colourImageData[i + 3] = maxFlatCameraImage > 0 ? 235 : 130; // A value
+      let normalizedIntensity = Math.floor(brightnessScaling * flatCameraImage[i/4]);
+      let [R, G, B] = colorMap[normalizedIntensity].map(x => 255 * x);
+
+      colourImageData[i + 0] = R;
+      colourImageData[i + 1] = G;
+      colourImageData[i + 2] = B;
+      colourImageData[i + 3] = maxFlatCameraImage > 0.01 ? 235 : 130; // A value
     }
     let imageData = new ImageData(colourImageData, 193, 258);
     ctxRef.current.putImageData(imageData, 0, 0);
   }, [flatCameraImage]);
+
   return (
     <>
       <canvas ref={canvasRef} className={"transform -scale-x-100"}></canvas>
